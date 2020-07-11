5c0bfa4b3b10a27a63be075cb80b3eac0537f13c
MichalPogodski <pogodskimichal@gmail.com>
2020-07-11 22:05:00 +0200

    localization + heuristic

--- agents/prob.py
+++ agents/prob.py
@@ -33,24 +33,23 @@ class LocAgent:
         self.eps_move = eps_move
 
         # previous action
-        self.prev_action = None
-        self.dir = None
-        self.prev_dir = None
+        self.prev_action = 'forward'
+
         prob = 1.0 / len(self.locations)
         self.P = prob * np.ones([len(self.locations), 4], dtype=np.float)
-        self.new_dir = ['N', 'N', 'N', 'N']
-        self.new_dir_prob = [0.0, 0.0, 0.0, 0.0]
+
+
 
 
     def __call__(self, percept):
         # update posterior
         # TODO PUT YOUR CODE HERE
+        dirs = ['N', 'E', 'S', 'W']
 
         # sensor prob
         sensor_prob = np.zeros((len(self.locations), 4), dtype=float)
         for idx, loc in enumerate(self.locations):
             prob = 1.0
-            dirs = ['N', 'E', 'S', 'W']
             for i, neig in enumerate(dirs):
                 neig_loc = nextLoc(loc, neig)
                 translated = []
@@ -59,22 +58,22 @@ class LocAgent:
                 pom_dict['right'] = 'E'
                 pom_dict['bckwd'] = 'S'
                 pom_dict['left'] = 'W'
-                if self.dir == 'N':
+                if neig == 'N':
                     pom_dict['fwd'] = 'N'
                     pom_dict['right'] = 'E'
                     pom_dict['bckwd'] = 'S'
                     pom_dict['left'] = 'W'
-                elif self.dir == 'E':
+                elif neig == 'E':
                     pom_dict['fwd'] = 'E'
                     pom_dict['right'] = 'S'
                     pom_dict['bckwd'] = 'W'
                     pom_dict['left'] = 'N'
-                elif self.dir == 'S':
+                elif neig == 'S':
                     pom_dict['fwd'] = 'S'
                     pom_dict['right'] = 'W'
                     pom_dict['bckwd'] = 'N'
                     pom_dict['left'] = 'E'
-                elif self.dir == 'W':
+                elif neig == 'W':
                     pom_dict['fwd'] = 'W'
                     pom_dict['right'] = 'N'
                     pom_dict['bckwd'] = 'E'
@@ -89,51 +88,41 @@ class LocAgent:
                         prob *= 0.1
                 else:
                     prob *= 1.0
-
             sensor_prob[idx] = [prob, prob, prob, prob]
-
 
         # transition prob
         transitions = np.zeros((len(self.locations), len(self.locations)), dtype=float)
         if self.prev_action == "forward":
             for idx, loc in enumerate(self.locations):
-                next_loc = nextLoc(loc, self.dir)
-                if (next_loc not in self.walls) and (legalLoc(next_loc, self.size)):
-                    next_idx = self.loc_to_idx[next_loc]
-                    transitions[idx, next_idx] = 0.95
-                    transitions[idx, idx] = 0.05
-                else:
-                    transitions[idx, idx] = 1.0
+                for i, dir in enumerate(dirs):
+                    if (nextLoc(loc, dir) not in self.walls) and (legalLoc(nextLoc(loc, dir), self.size)):
+                        next_loc = nextLoc(loc, dir)
+                        if (next_loc not in self.walls) and (legalLoc(next_loc, self.size)):
+                            next_idx = self.loc_to_idx[next_loc]
+                            transitions[idx, next_idx] = 0.95
+                            transitions[idx, idx] = 0.05
+                        else:
+                            transitions[idx, idx] = 1.0
         else:
             for idx, loc in enumerate(self.locations):
                 transitions[idx, idx] = 1.0
 
-
         # direction prob
-        self.prev_dir = self.dir
-        dir_prob = np.ones([4, 4], dtype=np.float)
+        dir_prob = np.zeros([4, 4], dtype=np.float)
         for i in range(4):
             if self.prev_action == "turnleft":
-                dir_prob[(i + 3) % 4, (i + 3) % 4] *= 0.95
-                dir_prob[i, i] *= 0.05
+                dir_prob[(i + 3) % 4, (i + 3) % 4] = 0.95
+                dir_prob[i, i] = 0.05
             elif self.prev_action == "turnright":
-                dir_prob[(i + 1) % 4, (i + 1) % 4] *= 0.95
-                dir_prob[i, i] *= 0.05
-        for i in range(4):
-            for j in range(4):
-                if dir_prob[i, j] == max(dir_prob[i]):
-                    self.new_dir[i] = dirs[i]
-                    self.new_dir_prob[i] = max(dir_prob[i])
-        for i in range(4):
-            if self.new_dir_prob[i] == max(self.new_dir_prob): self.dir = self.new_dir[i]
-
+                dir_prob[(i + 1) % 4, (i + 1) % 4] = 0.95
+                dir_prob[i, i] = 0.05
+            elif self.prev_action == "forward":
+                dir_prob[i, i] = 1.0
 
         self.P = np.dot(transitions.transpose(), self.P)
         self.P = sensor_prob * np.dot(self.P, dir_prob)
         self.P /= np.sum(self.P)
-
         # -----------------------
-
 
         action = 'forward'
         # TODO CHANGE THIS HEURISTICS TO SPEED UP CONVERGENCE
@@ -156,11 +145,9 @@ class LocAgent:
 
         # put probabilities in the array
         # TODO PUT YOUR CODE HERE
-        pom = np.zeros(len(self.locations), dtype = float)
         for idx, loc in enumerate(self.locations):
             P_arr[loc[0], loc[1]] = self.P[idx]
         # -----------------------
-
         return P_arr
 
     def forward(self, cur_loc, cur_dir):
